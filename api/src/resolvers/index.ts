2f8c8b271fd60a7855c42841f9241c837f71b604
rodriguezespinoza0795 <rodriguezespinoza0795@gmail.com>
2022-02-01 09:48:04 -0600

    Initial

---
+++ api/src/resolvers/index.ts
@@ -0,0 +1,14 @@
+import * as avo from './avocado.resolver'
+import * as scalars from './scalars'
+
+export default {
+    ...scalars,
+    Query: {
+        avo: avo.findOne,
+        avos: avo.findAll,
+    },
+    Mutation: {
+        createAvo: avo.createAvo,
+    },
+    Avocado: avo.resolver,
+}
