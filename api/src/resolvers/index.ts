db1ddbbf5e4f5e643b9fb1af01682bc72e3bc1d1
David Rodriguez <62265080+rodriguezespinoza0795@users.noreply.github.com>
2022-02-01 19:19:14 -0600

    DB MySQL

--- api/src/resolvers/index.ts
+++ api/src/resolvers/index.ts
@@ -1,14 +1,23 @@
+import type { Avocado } from '@prisma/client'
 import * as avo from './avocado.resolver'
 import * as scalars from './scalars'
 
 export default {
-    ...scalars,
-    Query: {
-        avo: avo.findOne,
-        avos: avo.findAll,
+  ...scalars,
+  BaseModel: {
+    __resolveType: (parent: Avocado) => {
+      if (parent.name) {
+        return 'Avocado'
+      }
+      return null // No more implementations
     },
-    Mutation: {
-        createAvo: avo.createAvo,
-    },
-    Avocado: avo.resolver,
+  },
+  Query: {
+    avo: avo.findOne,
+    avos: avo.findAll,
+  },
+  Mutation: {
+    createAvo: avo.createAvo,
+  },
+  Avocado: avo.resolver,
 }
